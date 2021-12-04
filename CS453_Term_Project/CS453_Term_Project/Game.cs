d9fab8b6bfd6719ac5f287613a9e2b7ce0c29cb4
augustus-loftin <56370257+augustus-loftin@users.noreply.github.com>
2021-12-04 15:29:12 -0800

    VS commit

---
+++ CS453_Term_Project/CS453_Term_Project/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace CS453_Term_Project
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gws, NativeWindowSettings nws)
+            : base(gws, nws)
+        {
+            this.CenterWindow(new Vector2i(1280, 768));
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(new Color4(0.3f, 0.4f, 0.5f, 1f));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
