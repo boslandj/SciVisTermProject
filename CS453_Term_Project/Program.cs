f31822c25d93e42b292ca194d811bf513c0a243d
augustus-loftin <56370257+augustus-loftin@users.noreply.github.com>
2021-12-04 15:51:16 -0800

    New Vs

---
+++ CS453_Term_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS453_Term_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
