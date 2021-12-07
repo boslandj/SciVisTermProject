3fec6fc31aaffd98d0509ec414b3bc4fc892b758
boslandj <boslandj@oregonstate.edu>
2021-12-07 14:05:44 -0800

    Update Program.cs

--- CS453_Term_Project/Program.cs
+++ CS453_Term_Project/Program.cs
@@ -5,63 +5,7 @@ using OpenTK.Windowing.Desktop;
 namespace CS453_Term_Project
 {
     class Program
-    {
-        struct Vertex
-        {
-                public float x;
-                public float y;
-                public float z;
-                public bool empty;
-        };
-
-        bool streamline_step(Vertex cpos, ref Vertex npos, bool forward, bool end)
-        {
-            //assigning that this vertex is not empty
-            npos.empty = false;
-
-            //setting up the vector components according to the field equation x = yz, y = xz, z = xy
-            float vectx = cpos.y * cpos.z;
-            float vecty = cpos.x * cpos.z;
-            float vectz = cpos.x * cpos.y;
-
-            //going forwards on the stream line
-            if (forward)
-            {
-
-                npos.x = cpos.x + ((float)0.1 * vectx);
-                npos.y = cpos.y + ((float)0.1 * vecty);
-                npos.z = cpos.z + ((float)0.1 * vectz);
-
-            }
-            //going backwards on the streamline
-            else
-            {
-
-                vectx *= -1;
-                vecty *= -1;
-                vectz *= -1;
-
-                npos.x = cpos.x + ((float)0.1 * vectx);
-                npos.y = cpos.y + ((float)0.1 * vecty);
-                npos.z = cpos.z + ((float)0.1 * vectz);
-
-            }
-            //testing the bounds for terminating the stream line
-            if ((npos.x < -500 || npos.x > 500) || (npos.y < -500 || npos.y > 500) || (npos.z < -500 || npos.z > 500))
-            {
-                //return end code
-                return false;
-
-            }
-            else
-            {
-                //return continue code
-                return true;
-
-            }
-
-            
-        }
+    { 
 
         static void Main(string[] args)
         {
