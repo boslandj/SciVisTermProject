4cc576b98ffe707802a81e2119e91bb71fe87ccd
augustus-loftin <56370257+augustus-loftin@users.noreply.github.com>
2021-12-09 16:51:51 -0800

    Comments

--- CS453_Term_Project/Game.cs
+++ CS453_Term_Project/Game.cs
@@ -25,12 +25,14 @@ namespace CS453_Term_Project
             this.empty = Empty;
         }
 
+        // Operator Overlaods
         public static Vertex operator *(Vertex a, Vertex b) => new Vertex(a.x * b.x, a.y * b.y, a.z * b.z, a.empty);
         public static Vertex operator +(Vertex a, Vertex b) => new Vertex(a.x + b.x, a.y + b.y, a.z + b.z, a.empty);
         public static Vertex operator -(Vertex a, Vertex b) => new Vertex(a.x - b.x, a.y - b.y, a.z - b.z, a.empty);
         public static Vertex operator *(float f, Vertex a) => new Vertex(a.x * f, a.y * f, a.z * f, a.empty);
         public static Vertex operator /(Vertex a, float f) => new Vertex(a.x / f, a.y / f, a.z / f, a.empty);
 
+        // Converts to Vector3
         public Vector3 ToVector3()
         {
             return new Vector3(this.x, this.y, this.z);
@@ -44,6 +46,11 @@ namespace CS453_Term_Project
         Vertex start1;
         Vertex start2;
 
+        private Color4 cv1 = new Color4(0.0f, 0.0f, 1.0f, 1.0f);
+        private Color4 cv2 = new Color4(0.8f, 0.8f, 1.0f, 1.0f);
+        private Color4 cv3 = new Color4(0.8f, 0.8f, 1.0f, 1.0f);
+        private Color4 cv4 = new Color4(0.0f, 0.0f, 1.0f, 1.0f);
+
         float erot = MathHelper.DegreesToRadians(3);
 
         public Surface(float x1, float y1, float z1, float x2, float y2, float z2, float Step)
@@ -52,15 +59,18 @@ namespace CS453_Term_Project
             start2 = new Vertex(x2, y2, z2, false);
             step = Step;
 
+            // calculate the number of verts imbetween the inital seed points
             float dist = MathF.Sqrt(MathF.Pow(x1 - x2, 2.0f) + MathF.Pow(y1 - y2, 2.0f) + MathF.Pow(z1 - z2, 2.0f));
             int stepCnt = (int)(dist / step);
             stepCnt *= 2;
 
+            // add inital seed points
             surf.Add(new List<Vertex>());
             surf.Add(new List<Vertex>());
             surf[0].Add(start1);
             surf[1].Add(start2);
 
+            // add seed points imbetween inital points
             for(int i = 1; i < stepCnt; i++)
             {
                 Vertex delta = start2 - start1;
@@ -71,9 +81,10 @@ namespace CS453_Term_Project
                 surf[i].Add(start1 + delta);
 
             }
-            Advance_Front();
+            Advance_Front(); // advance one so there will be atleas one full columb of quads
         }
 
+        // the vector field equation
         Vertex Vec_Fun(Vertex start)
         {
             Vector3 v;
@@ -89,6 +100,7 @@ namespace CS453_Term_Project
             return new Vertex(v.X, v.Y, v.Z, false);
         }
 
+        // calculates the distance between two verticies
         float Distance(Vertex A, Vertex B)
         {
             Vector3 v1 = A.ToVector3();
@@ -97,6 +109,7 @@ namespace CS453_Term_Project
             return (v1 - v2).Length;
         }
 
+        // angle between vertecies starting at S and between A and B
         float Angle(Vertex S, Vertex A, Vertex B)
         {
             Vector3 v1 = (S - A).ToVector3();
@@ -105,13 +118,15 @@ namespace CS453_Term_Project
             return MathF.Acos(Vector3.Dot(v1, v2) / (v1.Length * v2.Length));
         }
 
+        // check for divergance and add more geometry if needed
         private void Div()
         {
-            int i1 = 0;
-            int i2 = 1;
+            int i1 = 0; // index of the top of the quad, index 0 always point to the top of the first quad in the columb
+            int i2 = 1; // index of the bottom bottom of the quad
 
             while(i1 < surf.Count - 1)
             {
+                // find bottom of the quad
                 for(; i2 < surf.Count; i2++)
                 {
                     if (!surf[i2][surf[i2].Count - 2].empty)
@@ -130,8 +145,9 @@ namespace CS453_Term_Project
                     ln > (step / 2)
                     )
                 {
-                    if((i1 + 1) < i2)
+                    if((i1 + 1) < i2) // if there is an empty stream line between i1 and i2 use the empty for the extra geometry
                     {
+                        // add vertex at the midpoint between i1 and i2
                         Vertex v1 = surf[i1][surf[i1].Count - 1];
                         Vertex v2 = surf[i2][surf[i2].Count - 1];
 
@@ -147,8 +163,9 @@ namespace CS453_Term_Project
                         v3.empty = false;
                         surf[i1 + 1][surf[i1 + 1].Count - 2] = v3;
                     }
-                    else
+                    else // if there is not an empty stream line between i1 and i2 add a new stream line between i1 and i2
                     {
+                        // add new streamline
                         List<Vertex> sl = new List<Vertex>();
 
                         for(int i = 0; i < surf[0].Count; i++)
@@ -160,6 +177,7 @@ namespace CS453_Term_Project
 
                         i2++;
 
+                        // add vertex at the midpoint between i1 and i2
                         Vertex v1 = surf[i1][surf[i1].Count - 1];
                         Vertex v2 = surf[i2][surf[i2].Count - 1];
 
@@ -182,14 +200,16 @@ namespace CS453_Term_Project
             }
         }
 
+        // check for convergence and set streamline to empty if convergence present
         private void Con()
         {
-            int i1 = 0;
-            int i2 = 0;
-            int i3 = 0;
+            int i1 = 0; // index of the top of the first quad, index 0 always point to the top of the first quad in the columb
+            int i2 = 0; // index of the bottom of the first quad, index of the top of the second quad
+            int i3 = 0; // index of the bottom of the second quad
 
-            while(i1 < surf.Count - 1)
+            while (i1 < surf.Count - 1)
             {
+                // find the index i2
                 for (; i2 < surf.Count; i2++)
                 {
                     if (!surf[i2][surf[i2].Count - 2].empty)
@@ -197,6 +217,7 @@ namespace CS453_Term_Project
                 }
                 if (i2 >= surf.Count)
                     break;
+                // find the index i3
                 i3 = i2;
                 i3++;
                 for (; i3 < surf.Count; i3++)
@@ -217,7 +238,7 @@ namespace CS453_Term_Project
                     ln < (step / 2)
                     )
                 {
-
+                    // set the 2 vertecies bettween i1 and 13
                     Vertex v1 = surf[i2][surf[i2].Count - 1];
                     Vertex v2 = surf[i2][surf[i2].Count - 2];
 
@@ -226,6 +247,7 @@ namespace CS453_Term_Project
                     surf[i2][surf[i2].Count - 1] = v1;
                     surf[i2][surf[i2].Count - 2] = v2;
 
+                    // move the stream line point at i2 from the previous quads to the mid point betwen i1 and i3 of the previous quads to remove holes
                     if (surf[i2].Count >= 3)
                     {
                         Vertex v3 = surf[i1][surf[i1].Count - 3];
@@ -242,15 +264,17 @@ namespace CS453_Term_Project
             }
         }
 
+        // check and corect for right curvature
         private void Cur_Right()
         {
-            int i1 = 0;
-            int i2 = 1;
-            bool rot = false;
-            float prevlen = 0;
+            int i1 = 0; // index of the top of the quad, index 0 always point to the top of the first quad in the columb
+            int i2 = 1; // index of the bottom bottom of the quad
+            bool rot = false; // if any quads have need to be corrected for rotation
+            float prevlen = 0; // the length of the last quad that was shortened
 
             while (i1 < surf.Count - 1)
             {
+                // find i2
                 for (; i2 < surf.Count; i2++)
                 {
                     if (!surf[i2][surf[i2].Count - 2].empty)
@@ -272,6 +296,7 @@ namespace CS453_Term_Project
                     Vector3 vl = (surf[i1][surf[i1].Count - 1] - surf[i1][surf[i1].Count - 2]).ToVector3();
                     Vector3 vr = (surf[i2][surf[i2].Count - 1] - surf[i2][surf[i2].Count - 2]).ToVector3();
 
+                    // shorten vr to correct for rotation
                     vr.Normalize();
                     prevlen = A1 / (MathHelper.DegreesToRadians(90) + erot) * vl.Length;
                     vr =  prevlen * vr;
@@ -281,6 +306,7 @@ namespace CS453_Term_Project
 
                     surf[i2][surf[i2].Count - 1] = vertexR;
                 }
+                // if right rotation occurred and left rotation will not occur
                 else if(
                     rot &&
                     !(A1 > MathHelper.DegreesToRadians(90) + erot &&
@@ -294,22 +320,23 @@ namespace CS453_Term_Project
 
                     surf[i2][surf[i2].Count - 1] = vertexR;
                 }
-                //need to progress counters
+                // need to progress counters
                 i1 = i2;
                 i2++;
             }
         }
 
+        // check and corect for left curvature
         private void Cur_Left()
         {
-            int i1 = surf.Count - 1;
-            int i2 = surf.Count - 2;
-
-            float prevlen = 0;
-            bool rot = false;
+            int i1 = surf.Count - 1; // index of the top of the quad, index 0 always point to the top of the first quad in the columb
+            int i2 = surf.Count - 2; // index of the bottom bottom of the quad
+            float prevlen = 0; // if any quads have need to be corrected for rotation
+            bool rot = false; // the length of the last quad that was shortened
 
             while (i1 >= 0)
             {
+                // find i2
                 for (; i2 >= 0; i2--)
                 {
                     if (!surf[i2][surf[i2].Count - 2].empty)
@@ -331,6 +358,7 @@ namespace CS453_Term_Project
                     Vector3 vl = (surf[i1][surf[i1].Count - 1] - surf[i1][surf[i1].Count - 2]).ToVector3();
                     Vector3 vr = (surf[i2][surf[i2].Count - 1] - surf[i2][surf[i2].Count - 2]).ToVector3();
 
+                    // shorten vr to correct for rotation
                     vr.Normalize();
                     prevlen = A1 / (MathHelper.DegreesToRadians(90) + erot) * vl.Length;
                     vr = prevlen * vr;
@@ -340,6 +368,7 @@ namespace CS453_Term_Project
 
                     surf[i2][surf[i2].Count - 1] = vertexR;
                 }
+                // if right rotation occurred and left rotation will not occur
                 else if (
                     rot &&
                     !(A1 > MathHelper.DegreesToRadians(90) + erot &&
@@ -359,13 +388,14 @@ namespace CS453_Term_Project
             }
         }
 
+        // check and correct for high curvature
         private void Cur()
         {
             Cur_Right();
             Cur_Left();
         }
 
-
+        // advance surface by one columb
         public void Advance_Front()
         {
             for(int i = 0; i < surf.Count; i++)
@@ -377,6 +407,7 @@ namespace CS453_Term_Project
 
         }
 
+        // add a new vertex to one streamline
         public Vertex streamline_step(Vertex cpos)
         {
 
@@ -400,6 +431,7 @@ namespace CS453_Term_Project
             return cpos + vect;
         }
 
+        // consturct the integral surface
         public void build_surface(int iter)
         {
             for(int i = 0; i < iter; i++)
@@ -412,12 +444,14 @@ namespace CS453_Term_Project
             }
         }
 
+        // add a opengl vertex to a list
         private void add_vert_to_list(int idxs, int idxc, ref List<float> ves)
         {
             ves.Add(surf[idxs][idxc].x);
             ves.Add(surf[idxs][idxc].y);
             ves.Add(surf[idxs][idxc].z);
         }
+        // add a opengl color to a list
         private void add_color_to_list(float r, float g, float b, ref List<float> ves)
         {
             ves.Add(r);
@@ -425,18 +459,20 @@ namespace CS453_Term_Project
             ves.Add(b);
         }
 
+        // convert the integral surface to opengl arrays
         public void get_geo(out float[] verts, out uint[] indxs)
         {
             List<float> ves = new List<float>();
             List<uint> ins = new List<uint>();
 
-            for(int i = 0; i < surf[0].Count - 1; i++)
+            for (int i = 0; i < surf[0].Count - 1; i++)
             {
-                int j1 = 0;
-                int j2 = 1;
+                int j1 = 0; // index of the top of the quad, index 0 always point to the top of the first quad in the columb
+                int j2 = 1; // index of the bottom bottom of the quad
 
-                for(; j2 < surf.Count;)
+                for (; j2 < surf.Count;)
                 {
+                    // find bottom of the quad
                     while (j2 < surf.Count)
                     {
                         if (!(surf[j2][i].empty || surf[j2][i + 1].empty))
@@ -450,6 +486,7 @@ namespace CS453_Term_Project
 
                     uint ins_start = (uint)(ves.Count / 6);
 
+                    // add indicies of the opengl tiriangles to ins
                     ins.Add(ins_start);
                     ins.Add(ins_start + 1);
                     ins.Add(ins_start + 2);
@@ -457,20 +494,23 @@ namespace CS453_Term_Project
                     ins.Add(ins_start + 2);
                     ins.Add(ins_start + 3);
 
+                    // add verts and color of the quad to ves
                     add_vert_to_list(j1, i, ref ves);
-                    add_color_to_list(0.8f, 0.33f, 0.0f, ref ves);
+                    add_color_to_list(cv1.R, cv1.G, cv1.B, ref ves);
                     add_vert_to_list(j1, i + 1, ref ves);
-                    add_color_to_list(0.8f, 0.33f, 0.0f, ref ves);
+                    add_color_to_list(cv2.R, cv2.B, cv2.G, ref ves);
                     add_vert_to_list(j2, i + 1, ref ves);
-                    add_color_to_list(0.8f, 0.33f, 0.0f, ref ves);
+                    add_color_to_list(cv3.R, cv3.G, cv3.B, ref ves);
                     add_vert_to_list(j2, i, ref ves);
-                    add_color_to_list(0.8f, 0.33f, 0.0f, ref ves);
+                    add_color_to_list(cv4.R, cv4.G, cv4.B, ref ves);
 
+                    // swap j1 with j2 and inc j2
                     j1 = j2;
                     j2++;
                 }
             }
 
+            // convert ves and ins to opengl arrays and store them to verts and indxs
             verts = ves.ToArray();
             indxs = ins.ToArray();
         }
@@ -478,20 +518,10 @@ namespace CS453_Term_Project
 
     class Game : GameWindow
     {
-        private float[] vertices =
-        {
-             0.5f,  0.5f, 2.0f, // top right
-             0.5f, -0.5f, 2.0f, // bottom right
-            -0.5f, -0.5f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f, // top left
-        };
-
-        uint[] indices =
-        {
-            // Note that indices start at 0!
-            0, 1, 3, // The first triangle will be the bottom-right half of the triangle
-            1, 2, 3  // Then the second will be the top-right half of the triangle
-        };
+        private float[] vertices;
+        uint[] indices;
+        private float StepSize = 1.0f;
+        private int Advancements = 200;
 
         private int VertexBufferObject;
         private int VertexArrayObject;
@@ -515,17 +545,21 @@ namespace CS453_Term_Project
         {
             base.OnLoad();
 
+            // enable depth test
             GL.Enable(EnableCap.DepthTest);
             GL.DepthFunc(DepthFunction.Lequal);
             GL.DepthMask(true);
-
-            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             GL.DepthRange(0.0f, 1.0f);
 
+            // set back groun color
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            // setup the vertex buffer
             this.VertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, this.VertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, this.vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            //GL.BufferData(BufferTarget.ArrayBuffer, this.vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
             
+            // setup the vertex array for verts and colors
             this.VertexArrayObject = GL.GenVertexArray();
             GL.BindVertexArray(VertexArrayObject);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
@@ -533,14 +567,17 @@ namespace CS453_Term_Project
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
             GL.EnableVertexAttribArray(1);
 
+            // setup the element array
             this.elementBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, this.elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, this.indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+            //GL.BufferData(BufferTarget.ElementArrayBuffer, this.indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
 
+            // enable the shader
             this.shader = new Shader("..\\..\\..\\shader.Vert", "..\\..\\..\\shader.frag");
             this.shader.Use();
         }
 
+        // upate the vertex and index array
         public void UpdateGeometry(float[] Vert, uint[] Idx)
         {
             GL.BufferData(BufferTarget.ArrayBuffer, Vert.Length * sizeof(float), Vert, BufferUsageHint.StaticDraw);
@@ -550,13 +587,18 @@ namespace CS453_Term_Project
 
         protected override void OnUnload()
         {
+            // unbind buffers and arrays
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.BindVertexArray(0);
+
+            // unbind shader prog
             GL.UseProgram(0);
 
+            // delete buffers and arrays
             GL.DeleteBuffer(VertexBufferObject);
             GL.DeleteVertexArray(VertexArrayObject);
 
+            // remove shader
             shader.Dispose();
 
             base.OnUnload();
@@ -564,6 +606,7 @@ namespace CS453_Term_Project
 
         protected override void OnResize(ResizeEventArgs e)
         {
+            // update the view port and perspective transform when resized
             GL.Viewport(0, 0, e.Width, e.Height);
             Pers = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90), (float)e.Width / (float)e.Height, 0.0001f, 10000.0f);
             base.OnResize(e);
@@ -571,13 +614,12 @@ namespace CS453_Term_Project
 
         protected override void OnRenderFrame(FrameEventArgs args)
         {
+            // only draw the steam surface once
             if(drawStream)
             {
-                List<List<Vertex>> surface_arr = new List<List<Vertex>>();
+                Surface surface = new Surface(1.0f, 1.0f, 1.0f, 10.0f , 2.0f, 2.0f, StepSize);
 
-                Surface surface = new Surface(1.0f, 1.0f, 1.0f, 10.0f , 2.0f, 2.0f, 0.1f);
-
-                surface.build_surface(500);
+                surface.build_surface(Advancements);
                 surface.get_geo(out vertices, out indices);
 
                 this.UpdateGeometry(vertices, indices);
@@ -589,8 +631,8 @@ namespace CS453_Term_Project
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            // setup and use the shader
             this.shader.Use();
-
             this.shader.SetMatrix4("transform", CameraT);
 
             GL.BindVertexArray(VertexArrayObject);
@@ -604,6 +646,8 @@ namespace CS453_Term_Project
         {
             base.OnUpdateFrame(args);
 
+
+            // camera controles
             var mov_input = KeyboardState;
 
             Vector3 Translate = new Vector3();
---
+++ CS453_Term_Project/ReadMe.md
@@ -0,0 +1,28 @@
+﻿Camera controles
+W - Forward
+S - Backward
+A - Left
+D - Right
+Space - Up
+LAlt - Down
+LShift - go fast
+
+note: apologies but the speed is dependent on the framerate
+
+To change the number of advancement in the surface change Advancments in the Game class
+To change the sample rate of the surface change StepSize in the Game class
+
+to chagne the colors of the quads change cv1, cv2, cv3 and cv4 in the Surface class
+
+note: quad format
+					   Twards sead Point 1:
+
+						cv1 -------- cv2
+						 |            |
+						 |            |
+Towards Surface Start:	 |            |		Twards Surface End:
+						 |            |
+						 |            |
+						cv4 -------- cv3
+
+					   Twards sead Point 2:
